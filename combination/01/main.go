9b471d26152c6166f1291ef670fb281761ab27b9
lazytortoise <lazytortoise@163.com>
2022-03-22 20:08:48 +0800

    tmp save

--- combination/01/main.go
+++ combination/01/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /********************************
 抽奖系统。需要依次从 100 个人中，抽取三等奖 10 名，二等奖 3 名和一等奖 1 名。请列出所有可能的组合，需要注意的每人最多只能被抽中 1 次。
 排列和组合，组合没有去掉了顺序要求，解题思路应该和排列是一致的，但需要保存中奖的组合，进行去重
@@ -11,6 +13,25 @@ package main
 */
 
 func main() {
-	var people = [10]int{1, 2, 3, 4, 5, 6}
-
+	var people = []int{1, 2, 3, 4, 5, 6}
+	var recursion func(leftNums, tmpNums []int)
+	recursion = func(leftNums, tmpNums []int) {
+		if len(tmpNums) == 5 {
+			fmt.Printf("tmpNums:%v\r\n", tmpNums)
+			return
+		}
+		for _, pv := range people {
+			newTmpRes := append(tmpNums, pv)
+			newLeftNums := make([]int, len(leftNums)-1)
+			count := 0
+			for _, pv1 := range people {
+				if pv1 != pv {
+					newLeftNums[count] = pv1
+					count++
+				}
+			}
+			recursion(leftNums, newTmpRes)
+		}
+	}
+	recursion(people, make([]int, 0))
 }
